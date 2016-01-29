478cd3fabb6e2c021fc0f8fbee33b7c69cc9a4f1
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-01-29 17:20:56 +0000

    starting to create flexible game logic

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  
+
   console.log("jQuery has loaded");
 
   var $tileArray = $("li");
-  console.log($tileArray);
 
   var clickedIndex=0;
   var counter = 0;
+  var turnCounter = 0;
+
 
   var winnersObj = {"winningCombos":[
 
@@ -20,25 +21,20 @@ $(document).ready(function() {
     {"clickedTile":7, "relatedTiles":[4,6,8]},
     {"clickedTile":8, "relatedTiles":[5,7]}
 
-    ]}
+    ]} 
 
-    //try to make this into an array to consider diff sized boards without having to manually hardcode in related tiles... there must be a mathematical way!!!
-
-  
     // for each grid in my tilesArray, assign a event listener on mouse click  
     $tileArray.each(function (i, value) { 
 
       $($tileArray[i]).click(function() {
 
         clickedIndex = this.id;
-        // console.log(clickedIndex);
 
-        move(clickedIndex);
+        // move(clickedIndex);
 
-        hasWon();
+        // hasWon();
 
-
-
+        arrayRelationships();
 
       })
     })
@@ -61,107 +57,107 @@ $(document).ready(function() {
         var $tile = $(str);
         $tile.toggleClass("green");
       }
+      turnCounter ++;
+      console.log("You have had " + turnCounter + " turns!");
     }
 
 
     function hasWon(){
 
-      var className = $($tileArray[0]).attr('class');
-
       //iterate through the squares in tileArray. if they have class of green then add 1 to the hasWonCounter. outside of the loop function if the hasWonCounter is equal to the length of the tileArray then you've won!
 
       $tileArray.each(function (i, value) {
-        console.log($($tileArray[i]));
 
+        var hasClass = $($tileArray[i]).attr('class');  
 
-      if(className == "green"){
-        console.log("success. counter will now increment.");
-        counter ++;
-        console.log(counter);
+        if(hasClass === "green"){
+          counter ++;
+        }
+      });
+
+      if(counter == $tileArray.length) {
+        alert("YOU HAVE WON");
+        resetBoard();
+        return true;
       }
-
-      if(counter == $tileArray.length){
-        console.log("YOU'VE WON!!!!1");
         counter = 0;
+    };
+
+    function resetBoard() {
+
+      $tileArray.each(function (i, value) {
+
+        var removeGreen = $($tileArray[i]).removeClass("green");
+        turnCounter = 0;
+        counter = 0;
+
+      });
+    }
+
+    function arrayRelationships () {
+
+      //get the index of the clicked tile. then for this also find its related tiles by finding their indexes. Store these all indexes including the clicked tile index into an array which will be used to iterate over and add the toggle class functionality.
+      var myArray = [];
+      
+      var row_length = Math.sqrt($tileArray.length); 
+
+      var indexAsNumber = parseInt(clickedIndex);
+      myArray.push(indexAsNumber);
+      var p = typeof indexAsNumber;
+
+      var rel1 = indexAsNumber + 1;
+      var rel2 = indexAsNumber -1;
+      var rel3 = indexAsNumber - row_length;
+      var rel4 = indexAsNumber + row_length;
+
+      myArray.push(rel1,rel2,rel3,rel4);
+      console.log(myArray);
+
+      for (i=0;i<myArray.length; i++){
+
+        // console.log(myArray[i]);
+
+        //conditions which I need to account for - if on top row, bottom row, left row or right row do something different, otherwise iterate through all items myArray and toggle their class.
+
+        // for top row - if the index number in question is less than the rowLength (i.e.3 atm) then you know its on the top row. 
+
+        // for the bottom row -  
+
+        // for the left hand side - 
+
+        //for the right hand side - 
+
+        if(myArray[i] < 0 || myArray[i] > $tileArray.length){
+          // console.log(myArray[i] + " isn't being considered as it's not on the board");
+          // console.log(myArray.length + " is the length of the array");
+        } else{
+
+            var x = $tileArray[myArray[i]].id;
+            var str = "#"+x;
+            var $tile = $(str);
+            $tile.toggleClass("green");
+        }
+
       }
+    }
+
+    function checkifLeftRow() {
 
 
-    });
 
+
+    }
 
 
 
 
 });
 
+      //try to make this into an array to consider diff sized boards without having to manually hardcode in related tiles... there must be a mathematical way!!!
+
+      //work out how player 2 will play - player 1 goes first and after the first click a timer starts counting down. player 1's turn will end once the timer has run out, at which point their turn score will be logged to compare against player 2's if neither manages to win) . if the timer runs out before the hasWon function has returned true then player ones turn has ended and its onto player two. 
 
 
-
-
-
-
-
-
-
-
-
-    // $.each([myArr], function( index, value ) {
-    //   console.log( index + ": " + value );
-    // });
-
-      // $grid.each(function (i, value) { 
-
-      //   $($grid[i]).click(function() {
-
-      //     // console.log('div' + i+ ':' + $(this).attr('id')); 
-
-      //     if($(this).attr('id') == 1){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[1]).toggleClass("green");  
-      //       $($grid[3]).toggleClass("green");            
-      //     } else if ($(this).attr('id') == 2){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[0]).toggleClass("green");  
-      //       $($grid[2]).toggleClass("green");  
-      //       $($grid[4]).toggleClass("green");   
-      //     } else if ($(this).attr('id') == 3){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[1]).toggleClass("green");  
-      //       $($grid[5]).toggleClass("green");   
-      //     } else if ($(this).attr('id') == 4){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[0]).toggleClass("green");  
-      //       $($grid[6]).toggleClass("green");   
-      //       $($grid[4]).toggleClass("green");  
-      //     } else if ($(this).attr('id') == 5){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[1]).toggleClass("green");  
-      //       $($grid[3]).toggleClass("green");   
-      //       $($grid[5]).toggleClass("green");  
-      //       $($grid[7]).toggleClass("green"); 
-      //     } else if ($(this).attr('id') == 6){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[2]).toggleClass("green");  
-      //       $($grid[4]).toggleClass("green");   
-      //       $($grid[8]).toggleClass("green");   
-      //     } else if ($(this).attr('id') == 7){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[7]).toggleClass("green");  
-      //       $($grid[4]).toggleClass("green");     
-      //     } else if ($(this).attr('id') == 8){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[6]).toggleClass("green");  
-      //       $($grid[4]).toggleClass("green");   
-      //       $($grid[8]).toggleClass("green");   
-      //     } else if ($(this).attr('id') == 9){ 
-      //       $(this).toggleClass("green");
-      //       $($grid[7]).toggleClass("green");  
-      //       $($grid[5]).toggleClass("green");     
-      //     } 
-
-      //   })
-
-      // });
 
 
 
