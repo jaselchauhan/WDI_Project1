d84fb5b99017b7c40544154238d679c16ae96ef9
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-01-30 00:54:20 +0000

    initial css styling started

--- js/app.js
+++ js/app.js
@@ -2,30 +2,29 @@ $(document).ready(function() {
 
   console.log("jQuery has loaded");
 
+  animateBackground();
+
   var $tileArray = $("li");
   var row_length = Math.sqrt($tileArray.length); 
 
   var clickedIndex=0;
-  var GreenSquarecounter = 0;
+  var greenTileCount = 0;
   var turnCounter = 0;
   var player1score = 0;
 
   var $scoreBoard = $("#score");
   var $reset = $("#reset");
 
-
-
-
     $tileArray.each(function (i, value) { 
 
       $($tileArray[i]).click(function() {
 
-        clickedIndex = this.id;   
-        $( "h1").fadeToggle( "slow", "linear" );
-        $(this).fadeOut(70);     
-        $(this).fadeIn(200);  
-
+      clickedIndex = this.id;   
+         
         move(isTopRow,isBottomRow,isLeftColumn,isRightColumn, clickedIndex);
+
+        $(this).fadeOut(250);     
+        $(this).fadeIn(300); 
 
         hasWon();
 
@@ -39,26 +38,48 @@ $(document).ready(function() {
 
     });
 
+
+
+
+
+/*
+----------------------------------------------------------------------------------------------------------------
+    ____                 __  _                 
+   / __/_  ______  _____/ /_(_)___  ____  _____
+  / /_/ / / / __ \/ ___/ __/ / __ \/ __ \/ ___/
+ / __/ /_/ / / / / /__/ /_/ / /_/ / / / (__  ) 
+/_/  \__,_/_/ /_/\___/\__/_/\____/_/ /_/____/  
+                                               
+----------------------------------------------------------------------------------------------------------------
+*/
+
+    function animateTiles(clickedIndex) {
+      $(this).fadeOut(70);     
+      $(this).fadeIn(200); 
+    }
+
+    function animateBackground(){
+    $('body').ambience();  
+    };
+    
     function hasWon(){
 
       $tileArray.each(function (i, value) {
-
-        var hasClass = $($tileArray[i]).attr('class');  
-
-        if(hasClass === "green"){
-          GreenSquarecounter ++;
+        if($($tileArray[i]).attr('class') === "green"){
+          greenTileCount ++;
         }
       });
 
-      if(GreenSquarecounter == $tileArray.length) {
+      if(greenTileCount == $tileArray.length) {
         alert("YOU HAVE WON");
         player1score = turnCounter;
         resetBoard();
         console.log(player1score + " is the final score for p1");
         return true;
       }
-        GreenSquarecounter = 0;
+        greenTileCount = 0;
     };
+
 
     function resetBoard() {
 
@@ -66,13 +87,13 @@ $(document).ready(function() {
 
         var removeGreen = $($tileArray[i]).removeClass("green");
         turnCounter = 0;
-        GreenSquarecounter = 0;
+        greenTileCount = 0;
 
       });
     }
 
-    function isTopRow (clickedIndex){
-     
+    function isTopRow(clickedIndex) {
+    
       if(parseInt(clickedIndex) <= row_length-1){
         return true;
       } else {
@@ -116,79 +137,68 @@ $(document).ready(function() {
 
     function move (isTopRow,isBottomRow,isLeftColumn,isRightColumn, clickedIndex) {
 
-      var myArray = [];
-      console.log(myArray);
+      var currentClick = [];
+      console.log(currentClick);
 
       var indexAsNumber = parseInt(clickedIndex);
-      myArray.push(indexAsNumber);
-      
+      currentClick.push(indexAsNumber);
 
       if(isTopRow(clickedIndex)==true && isLeftColumn(clickedIndex)==true){
-        // console.log("top row condition true. zeroth index logged");
         var rel1 = indexAsNumber + 1;
         var rel4 = indexAsNumber + row_length;
-        myArray.push(rel1,rel4);
-        console.log(myArray);
+        currentClick.push(rel1,rel4)
         turnCounter ++;
       } else if (isTopRow(clickedIndex)==true && isRightColumn(clickedIndex)==true) {
           var rel2 = indexAsNumber -1;
           var rel4 = indexAsNumber + row_length;
-          myArray.push(rel2,rel4);
-          console.log(myArray);
+          currentClick.push(rel2,rel4);
           turnCounter ++;
       } else if (isBottomRow(clickedIndex)==true && isLeftColumn(clickedIndex)==true){
           var rel1 = indexAsNumber + 1;
           var rel3 = indexAsNumber - row_length;
-          myArray.push(rel1,rel3);
-          console.log(myArray);
+          currentClick.push(rel1,rel3);
           turnCounter ++;
       } else if (isBottomRow(clickedIndex)==true && isRightColumn(clickedIndex)==true){
           var rel2 = indexAsNumber -1;
           var rel3 = indexAsNumber - row_length;
-          myArray.push(rel2,rel3);
-          console.log(myArray);
+          currentClick.push(rel2,rel3);
           turnCounter ++;
       } else if (isTopRow(clickedIndex)==true) {
           var rel1 = indexAsNumber + 1;
           var rel2 = indexAsNumber -1;
           var rel4 = indexAsNumber + row_length;
-          myArray.push(rel1,rel2,rel4);
-          console.log(myArray);
+          currentClick.push(rel1,rel2,rel4);
           turnCounter ++;
       } else if (isBottomRow(clickedIndex)==true){
           var rel1 = indexAsNumber + 1;
           var rel2 = indexAsNumber -1;
           var rel3 = indexAsNumber - row_length;
-          myArray.push(rel1,rel2,rel3);
-          console.log(myArray);
+          currentClick.push(rel1,rel2,rel3);
           turnCounter ++;
       } else if (isLeftColumn(clickedIndex)==true){
           var rel1 = indexAsNumber + 1;
           var rel3 = indexAsNumber - row_length;
           var rel4 = indexAsNumber + row_length;
-          myArray.push(rel1,rel3,rel4);
-          console.log(myArray);
+          currentClick.push(rel1,rel3,rel4);
           turnCounter ++;
       } else if (isRightColumn(clickedIndex)==true){
           var rel2 = indexAsNumber -1;
           var rel3 = indexAsNumber - row_length;
           var rel4 = indexAsNumber + row_length;
-          myArray.push(rel2,rel3,rel4);
-          console.log(myArray);
+          currentClick.push(rel2,rel3,rel4);
           turnCounter ++;
       } else {
           var rel1 = indexAsNumber + 1;
           var rel2 = indexAsNumber -1;
           var rel3 = indexAsNumber - row_length;
           var rel4 = indexAsNumber + row_length;
-          myArray.push(rel1,rel2,rel3,rel4);
-          console.log(myArray);
+          currentClick.push(rel1,rel2,rel3,rel4);
           turnCounter ++;
       }
 
-      for (i=0;i<myArray.length; i++){
+      for (i=0;i<currentClick.length; i++){
 
-            var x = $tileArray[myArray[i]].id;
+            var x = $tileArray[currentClick[i]].id;
             var str = "#"+x;
             var $tile = $(str);
             $tile.toggleClass("green");
@@ -199,11 +209,6 @@ $(document).ready(function() {
 
 });
 
-
-      //work out how player 2 will play - player 1 goes first and after the first click a timer starts counting down. player 1's turn will end once the timer has run out, at which point their turn score will be logged to compare against player 2's if neither manages to win) . if the timer runs out before the hasWon function has returned true then player ones turn has ended and its onto player two.
-
-      //add timer function which starts from the first mouse click. then wokrk out how the game is actually won - ie by least number of moves? or by best time with completed
-      // add a reset board button 
 
 
 
