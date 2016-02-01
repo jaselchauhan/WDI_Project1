d75d483067b155724df4014560edc1aa029d67ae
Jasel Chauhan <jasel.chauhan@gmail.com>
2016-02-01 10:17:24 +0000

    tweaking game logic

--- js/app.js
+++ js/app.js
@@ -30,7 +30,9 @@ $(document).ready(function() {
   var startingClicksIndex = [8,15,35,50,75,100];
   var animateRelTileArray = [];
   var livesCounter = 3;
-  var whichPlayer = 0;
+
+
+  // var whichPlayer = 0;
   var grid3score = 0;
   var grid4score = 0;
   var grid5score = 0;
@@ -38,12 +40,10 @@ $(document).ready(function() {
   var grid7score = 0;
   var grid8score = 0;
 
-  var ScoreBoardObj = [];
  //main code block. runs on each lite click.
   $('.grid').on("click", 'li', function(li) {
       
           // clickSound();
-
           clickedIndex = this.id;
 
           if(!gameOver()){ 
@@ -66,14 +66,13 @@ $(document).ready(function() {
     $($reset).click(function() {
       resetBoard();
       $scoreBoard.html("----------");
-      console.log(livesCounter+" before displaying on dom");
+      // console.log(livesCounter+" before displaying on dom");
       if(livesCounter > 0){
-      $p1score.html("wise decision...maybe it's time for another level? | Remaining lives: " + (livesCounter-1));
-      } else if (livesCounter === 0) {
-        $p1score.html("unlucky, that's game over...you can't beat me that easily! Though you might be able to after a few tries!");
-      }
       livesCounter -=1;
-      console.log("you have " + livesCounter + " lives remaining..");
+      $p1score.html("wise decision...maybe it's time for another level? | Remaining lives: " + livesCounter);
+      console.log(livesCounter + " is your remaining life count");
+      } else if (livesCounter === 0) {
+        $p1score.html("unlucky, that's game over...you can't beat me that easily! Though you might be able to after a few tries!")}
 
       $gridSizeSelector.show();
       $('#welcome').delay(1000).fadeIn();
@@ -138,38 +137,17 @@ $(document).ready(function() {
 ----------------------------------------------------------------------------------------------------------------
 */  
 
-//socreboard using row length to calc grid size and then store in relvant array/obj. then call this and display as a Dom element when lives remaining = 0;
 
-  function addToScoreBoard(){
-    //if array length is 9 then add then final score to the array/object position related to the 3X3 level score and push value there. 
-    var arrayLength = $("li").length;
-    if (arrayLength ===9){
-      grid3score= player1score;
-      console.log(grid3score);
-      $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-      // $p1score.html("3X3: " + grid3score);  
-    } else if (arrayLength ===16){
-      grid4score= player1score;
-      console.log(grid4score);
-      $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-     } else if (arrayLength ===25){
-        grid5score= player1score;
-        console.log(grid5score);
-        $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-      } else if (arrayLength ===36){
-        grid6score= player1score;
-        console.log(grid6score);
-        $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-      } else if (arrayLength ===49){
-        grid7score= player1score;
-        console.log(grid7score);
-        $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-      } else if (arrayLength ===64){
-        grid8score= player1score;
-        console.log(grid8score);
-        $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
-      }
-  }
+  // this function checks the remaining lives left at the start of each turn. If there are no lives left the console will log.
+  function checkLivesRemaining(livesCounter){
+    if(livesCounter <= 0){
+      console.log("lives have run out! please restart browser and try again");
+      return true;
+    } else {
+
+      return false;
+    }
+  } 
 
   function prepBoard(){
     resetBoard();
@@ -178,18 +156,11 @@ $(document).ready(function() {
     $('#welcome').delay(400).fadeOut();
   };
 
-  function clickSound(){
-    var audio = document.getElementById("clickSound");
-    audio.play();
-  }                
+  // function clickSound(){
+  //   var audio = document.getElementById("clickSound");
+  //   audio.play();
+  // }                
 
-  function whichPlayer(gameOver) {
-    if(gameOver){
-      return "player2";
-    } else {
-      return "player1";
-    }
-  }
 
   function gameOver(){
     if(livesCounter<=0){
@@ -216,78 +187,6 @@ $(document).ready(function() {
     }
   }
 
-  function create8X8 () {
-
-    for (i=0;i<64;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-      $tileArray = $("li");
-      row_length = Math.sqrt($tileArray.length); 
-      $($tileArray).css("height", "10%").css("width", "10%");
-  }
-
-  function create7X7 () {
-    for (i=0;i<49;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-    $tileArray = $("li");
-    row_length = Math.sqrt($tileArray.length);
-    $($tileArray).css("height", "12%").css("width", "12%");
-
-  }
-
-  function create6X6 () {
-    for (i=0;i<36;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-    $tileArray = $("li");
-    row_length = Math.sqrt($tileArray.length);
-    $($tileArray).css("height", "14%").css("width", "14%");
-
-  }
-
-  function create5X5 () {
-    for (i=0;i<25;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-    $tileArray = $("li");
-    row_length = Math.sqrt($tileArray.length);
-    $($tileArray).css("height", "18%").css("width", "18%");
-
-  }
-
-  function create4X4 () {
-    for (i=0;i<16;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-    $tileArray = $("li");
-    row_length = Math.sqrt($tileArray.length);
-    $($tileArray).css("height", "23%").css("width", "23%");
-
-  }
-
-
-  function create3X3 () {
-    for (i=0;i<9;i++){
-      var newSquare = "<li id="+parseInt(i)+"></li>";
-      $(".grid").append(newSquare);
-    }
-
-    $tileArray = $("li");
-    row_length = Math.sqrt($tileArray.length);
-    $($tileArray).css("height", "31%").css("width", "31%");
-
-  }
   //deletes the DOM elements which currently exist. Use in conjuction with create Grid functions so you effectively remove and add new DOM elements. I think this is what is breaking my current game logic - investigate further.
     function removeGrid() {
       $tileArray.each(function (i, value) { 
@@ -313,15 +212,10 @@ $(document).ready(function() {
       colors: ['black','darkgrey','darkblue', 'cyan','black','darkgrey','darkblue','cyan']}
       );  
     };
-
-    function whoseTurn(whichPlayer){
-      if(whichPlayer%2==0){
-        return "p2";
-      } else {
-        return "p1";
-      }
-    }
     
+
+    // checks the game board to see if all lites are green. if all lites are green the game has been won and the winning sequence appears with message and score for level. score is passed into the scoreboard and the green tile count is reset back to 0.
+
     function hasWon(){
 
       $tileArray.each(function (i, value) {
@@ -335,7 +229,7 @@ $(document).ready(function() {
       if(greenTileCount == $tileArray.length) {
         $('#welcome').delay(400).fadeIn();
         $(".grid").fadeOut(2000);
-        $(".grid").fadeIn(20);
+        $(".grid").fadeIn(50);
         player1score = turnCounter;
         resetBoard();
 
@@ -346,17 +240,13 @@ $(document).ready(function() {
         $gridSizeSelector.show(); 
         $('#welcome').delay(4500).fadeOut();
         $p1score.html("you've won! " + player1score + " is the score for this level...choose next level to play");
-        // $gridSizeSelector.delay(3000).hide(); 
-        p1score = 
-
-        $("#title").html(whoseTurn() + " scored " + player1score + "!...next player's go");
-        whichPlayer +=1;
-        $("#title").delay(4000).html("litesweeper");
 
         return true;
       }
         greenTileCount = 0;
     };
+
+
 
     // resets the board back to red and the counters back to 0
     function resetBoard() {
@@ -366,55 +256,11 @@ $(document).ready(function() {
         var removeGreen = $($tileArray[i]).removeClass("green");
         turnCounter = 0;
         greenTileCount = 0;
-        $("#title").html("litesweeper");
-
       });
     }
 
-    //checks if clicked square is on the top row of the grid and returns a boolean statement to be passed into move function
-    function isTopRow(clickedIndex) {
-    
-      if(parseInt(clickedIndex) <= row_length-1){
-        return true;
-      } else {
-        return false;
-      }
-    }
-    //checks if clicked square is on the bottom row of the grid and returns a boolean statement to be passed into move function
-    function isBottomRow(clickedIndex){
-      if(parseInt(clickedIndex) > $tileArray.length - (row_length+1)) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-    //checks if clicked square is in the left of the grid and returns a boolean statement to be passed into move function
-    function isLeftColumn(clickedIndex){
 
-      if(parseInt(clickedIndex) ==0 ){
-        return true;
-      }
-      else if(parseInt(clickedIndex)%(row_length) ==0){
-        return true;
-      } else {
-        return false;
-      }
-    }
-    //checks if clicked square is in the right of the grid and returns a boolean statement to be passed into move function
-    function isRightColumn(clickedIndex){
-
-      if(parseInt(clickedIndex) === $tileArray.length -1 ){
-        return true;
-      }
-
-      else if((parseInt(clickedIndex) + 1) % row_length == 0){
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-    //logic for turning the right grids to green 
+    //logic for turning the right grids to green. takes in 5 arguments - 4 of which are to check for where the borders of the grid lie, and the final argument is the clicked lite index number.
     function move (isTopRow,isBottomRow,isLeftColumn,isRightColumn, clickedIndex) {
 
       var currentClick = [];
@@ -426,8 +272,6 @@ $(document).ready(function() {
       var rel2 = indexAsNumber -1;
       var rel3 = indexAsNumber - row_length;
       var rel4 = indexAsNumber + row_length;
-
-
 
       if(isTopRow(clickedIndex) && isLeftColumn(clickedIndex)){
         currentClick.push(rel1,rel4);
@@ -469,6 +313,159 @@ $(document).ready(function() {
             $tile.toggleClass("green");
             $tile.fadeIn(300);
 
+        }
+    }
+
+    //checks if clicked square is on the top row of the grid and returns a boolean statement to be passed into move function
+    function isTopRow(clickedIndex) {
+    
+      if(parseInt(clickedIndex) <= row_length-1){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    //checks if clicked square is on the bottom row of the grid and returns a boolean statement to be passed into move function
+    function isBottomRow(clickedIndex){
+      if(parseInt(clickedIndex) > $tileArray.length - (row_length+1)) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    //checks if clicked square is in the left of the grid and returns a boolean statement to be passed into move function
+    function isLeftColumn(clickedIndex){
+
+      if(parseInt(clickedIndex) ==0 ){
+        return true;
+      }
+      else if(parseInt(clickedIndex)%(row_length) ==0){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    //checks if clicked square is in the right of the grid and returns a boolean statement to be passed into move function
+    function isRightColumn(clickedIndex){
+
+      if(parseInt(clickedIndex) === $tileArray.length -1 ){
+        return true;
+      }
+
+      else if((parseInt(clickedIndex) + 1) % row_length == 0){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+
+    function create8X8 () {
+
+      for (i=0;i<64;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+        $tileArray = $("li");
+        row_length = Math.sqrt($tileArray.length); 
+        $($tileArray).css("height", "10%").css("width", "10%");
+    }
+
+    function create7X7 () {
+      for (i=0;i<49;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+      $tileArray = $("li");
+      row_length = Math.sqrt($tileArray.length);
+      $($tileArray).css("height", "12%").css("width", "12%");
+
+    }
+
+    function create6X6 () {
+      for (i=0;i<36;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+      $tileArray = $("li");
+      row_length = Math.sqrt($tileArray.length);
+      $($tileArray).css("height", "14%").css("width", "14%");
+
+    }
+
+    function create5X5 () {
+      for (i=0;i<25;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+      $tileArray = $("li");
+      row_length = Math.sqrt($tileArray.length);
+      $($tileArray).css("height", "18%").css("width", "18%");
+
+    }
+
+    function create4X4 () {
+      for (i=0;i<16;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+      $tileArray = $("li");
+      row_length = Math.sqrt($tileArray.length);
+      $($tileArray).css("height", "23%").css("width", "23%");
+
+    }
+
+
+    function create3X3 () {
+      for (i=0;i<9;i++){
+        var newSquare = "<li id="+parseInt(i)+"></li>";
+        $(".grid").append(newSquare);
+      }
+
+      $tileArray = $("li");
+      row_length = Math.sqrt($tileArray.length);
+      $($tileArray).css("height", "31%").css("width", "31%");
+
+    }
+
+    //socreboard using row length to calc grid size and then store in relvant array/obj. then call this and display as a DOM element when lives remaining = 0;
+
+    function addToScoreBoard(){
+
+        var arrayLength = $("li").length;
+
+        if (arrayLength ===9){
+          grid3score= player1score;
+          console.log(grid3score);
+          $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
+        } else if (arrayLength ===16){
+          grid4score= player1score;
+          console.log(grid4score);
+          $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
+        } else if (arrayLength ===25){
+            grid5score= player1score;
+            console.log(grid5score);
+            $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
+        } else if (arrayLength ===36){
+            grid6score= player1score;
+            console.log(grid6score);
+            $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
+        } else if (arrayLength ===49){
+            grid7score= player1score;
+            console.log(grid7score);
+            $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
+        } else if (arrayLength ===64){
+            grid8score= player1score;
+            console.log(grid8score);
+            $playerScoreboard.html("3X3 score: " + grid3score + " | " + "4X4 score: " + grid4score + " | " +"5X5 score: " + grid5score + " | " +"6X6 score: " + grid6score + " | " +"7X7 score: " + grid7score + " | "+"8X8 score: " + grid8score);
         }
     }
 
